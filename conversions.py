6691a62744348de15a4ab43ea7ed52956cedae46
Rado <rrafiringa@gmail.com>
2016-03-08 22:32:34 -0500

    Update conversions.py
    Conversion functions setup.

--- conversions.py
+++ conversions.py
@@ -4,3 +4,24 @@
 """
 Conversion functions
 """
+
+def fahr_to_celsius(fahr):
+    pass
+
+def fahr_to_kelvin(fahr):
+    pass
+
+def celsius_to_fahr(celsius):
+    pass
+
+def celsius_to_kelvin(celsius):
+    pass
+
+def kelvin_to_fahr(kelvin):
+    pass
+
+def kelvin_to_celsius(kelvin):
+    pass
+
+
+
